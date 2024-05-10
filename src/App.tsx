1eead0f598034c4a4a11f07eea6dc1142236f6c0
summermong <2514o_o@naver.com>
2024-05-10 23:49:26 +0900

    :sparkles: FEAT: QueryClientProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './App.css';
 import Home from './Pages/Home';
 import Frame from './design/Frame';
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Frame>
-      <Home />
-    </Frame>
+    <QueryClientProvider client={queryClient}>
+      <Frame>
+        <Home />
+      </Frame>
+    </QueryClientProvider>
   );
 }
 
