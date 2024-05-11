ae256ccd47973a2f3353cbd85c821d9d7a46c880
summermong <2514o_o@naver.com>
2024-05-11 23:47:54 +0900

    :sparkles: FEAT: TanStack Query 적용

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,42 +1,32 @@
-import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import FetchAddress from '../api/FetchAddress';
 
 const Home = () => {
-  const [address, setAddress] = useState('');
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['address'],
+    queryFn: FetchAddress,
+  });
 
-  if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+  let content;
 
-      const apiKey = import.meta.env.VITE_Google_API_KEY;
+  if (isPending) {
+    content = <div>Loading...</div>;
+  }
 
-      fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}&language=en`
-      )
-        .then(response => response.json())
-        .then(data => {
-          if (data.status === 'OK') {
-            const addressData = data.results[4].address_components[0].long_name;
-            console.log(data);
-            setAddress(addressData);
-          } else {
-            console.log('주소를 가져오는 데 문제가 발생했습니다.');
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    });
-  } else {
-    console.log('Geolocation을 지원하지 않습니다.');
+  if (isError) {
+    content = <div>error</div>;
+  }
+
+  if (data) {
+    content = <div>{data as string}</div>;
   }
 
   return (
     <div>
       <div className="text-right">⚙️</div>
-      <h1 className="font-semibold  text-3xl mb-3">Questrip</h1>
+      <h1 className="font-semibold text-3xl mb-3">Questrip</h1>
       <div className="flex justify-between items-center">
-        <div>{address}</div>
+        {content}
         <button className="ml-2 bg-mainColor rounded-xl text-center w-20 p-1 cursor-pointer">
           Change
         </button>
