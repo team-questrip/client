8ef1642c7c63d936dbb3d9efc73e729f764afc44
summermong <2514o_o@naver.com>
2024-05-21 10:59:43 +0900

    :sparkles: FEAT: DetailPage 작업중...

--- src/Pages/DetailPage.tsx
+++ src/Pages/DetailPage.tsx
@@ -1,17 +1,4 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
-
 const DetailPage = () => {
-  const latitudeValue = useSelector(
-    (state: RootState) => state.addressData.latitude
-  );
-  const longitudeValue = useSelector(
-    (state: RootState) => state.addressData.longitude
-  );
-
-  console.log(latitudeValue);
-  console.log(longitudeValue);
-
   return (
     <div>
       <header className="pb-3">⬅️</header>
