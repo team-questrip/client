6344e2ddc14f6cf93376ca3af7fd05368cd40d3a
summermong <2514o_o@naver.com>
2024-05-21 10:58:35 +0900

    :sparkles: FEAT: 장소 정보 받아오는 fetchPlaceDate API

--- src/api/FetchPlace.tsx
+++ src/api/FetchPlace.tsx
@@ -1,19 +1,22 @@
+import { GetPlaceProps } from '../interface/props';
 import { axiosInstance } from './axiosInstance';
 
-const fetchPlaceData = async () => {
+const fetchPlaceData = async (): Promise<GetPlaceProps['data']> => {
   const latitude = localStorage.getItem('latitude');
   const longitude = localStorage.getItem('longitude');
 
-  try {
-    const response = await axiosInstance.get(
-      `api/v1/place?latitude=${latitude}&longitude=${longitude}&page=0&size=10`
-    );
-    if (response.status === 200) {
-      return response.data;
-    }
-  } catch (error) {
-    console.error('API 요청 중 오류 발생:', error);
-    throw error;
+  if (!latitude || !longitude) {
+    throw new Error('위치 정보가 없습니다.');
+  }
+
+  const response = await axiosInstance.get<GetPlaceProps>(
+    `api/v1/place?latitude=${latitude}&longitude=${longitude}&page=0&size=10`
+  );
+
+  if (response.status === 200) {
+    return response.data.data;
+  } else {
+    throw new Error('Unexpected response status');
   }
 };
 
