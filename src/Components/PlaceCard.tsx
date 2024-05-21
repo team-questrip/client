99806e655f0c0997f010d4cb6e70616c090eb6b9
summermong <2514o_o@naver.com>
2024-05-21 10:59:09 +0900

    :sparkles: FEAT: placeCard 내용 조회

--- src/Components/PlaceCard.tsx
+++ src/Components/PlaceCard.tsx
@@ -4,42 +4,31 @@ import { PlaceCardProps } from '../interface/props';
 const PlaceCard: React.FC<PlaceCardProps> = ({ placeData }) => {
   const navigate = useNavigate();
 
-  const handleGoToDetail = () => {
-    if (placeData && placeData.data.content.length > 0) {
-      const placeId = placeData.data.content[0].id;
-      navigate(`/${placeId}`);
-    }
+  const handleGoToDetail = (id: string) => {
+    navigate(`/${id}`);
   };
 
   return (
     <div className="font-semibold pt-1 text-lg">
-      {placeData && placeData.data.content.length > 0 && (
-        <div
-          className="
-            mt-4 rounded-lg
-            shadow-md bg-white
-            cursor-pointer
-          "
-          onClick={handleGoToDetail}
-        >
-          <img
-            src={placeData.data.content[0].images[1].url}
-            className="rounded-lg"
-          />
-          <div className="p-4">
-            <div className="pt-2 font-semibold">
-              {placeData.data.content[0].placeName}
-            </div>
-            <div className="text-mainTextColor text-sm">
-              <div>
-                {placeData.data.content[0].content.recommendationReason}
-              </div>
-              <div>
-                Distance: {placeData.data.content[0].distance.toFixed(1)}mi
+      {placeData && placeData.content.length > 0 ? (
+        placeData.content.map(place => (
+          <div
+            key={place.id}
+            className="mt-4 rounded-lg shadow-md bg-white cursor-pointer"
+            onClick={() => handleGoToDetail(place.id)}
+          >
+            <img src={place.images[0]?.url} className="rounded-lg" />
+            <div className="p-4">
+              <div className="pt-2 font-semibold">{place.placeName}</div>
+              <div className="text-mainTextColor text-sm">
+                <div>{place.content.recommendationReason}</div>
+                <div>Distance: {place.distance.toFixed(1)}mi</div>
               </div>
             </div>
           </div>
-        </div>
+        ))
+      ) : (
+        <div>No places found</div>
       )}
     </div>
   );
