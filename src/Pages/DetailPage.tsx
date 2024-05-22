2d886aa0bbb38921935751a9d99ee65be24e8f5e
summermong <2514o_o@naver.com>
2024-05-22 16:25:25 +0900

    :sparkles: FEAT: DetailPage UI 및 로직 구성

--- src/Pages/DetailPage.tsx
+++ src/Pages/DetailPage.tsx
@@ -1,8 +1,52 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import FetchPlaceDetail from '../api/FetchPlaceDetail';
+import { IoArrowBack } from 'react-icons/io5';
+import PlaceDetail from '../components/PlaceDetail';
+
 const DetailPage = () => {
+  const { placeId } = useParams<{ placeId: string }>();
+
+  const {
+    data: detailPlaceData,
+    isLoading: isDetailPlaceLoading,
+    isError: isDetailPlaceError,
+    error: detailPlaceError,
+  } = useQuery({
+    queryKey: ['detailPlace', placeId],
+    queryFn: () => FetchPlaceDetail({ placeId: placeId! }),
+    enabled: !!placeId,
+  });
+
+  const navigate = useNavigate();
+
+  const handleBackPage = () => {
+    navigate(-1);
+  };
+
+  let content;
+
+  if (isDetailPlaceLoading) {
+    content = <div className="text-center">Loading...</div>;
+  }
+
+  if (isDetailPlaceError) {
+    content = <div>{detailPlaceError.message}</div>;
+  }
+
+  if (detailPlaceData) {
+    content = <PlaceDetail detailPlaceData={detailPlaceData} />;
+  }
+
   return (
     <div>
-      <header className="pb-3">⬅️</header>
-      <div>dd</div>
+      <header className="pb-3">
+        <IoArrowBack
+          onClick={handleBackPage}
+          className=" cursor-pointer size-6 hover:scale-125"
+        />
+      </header>
+      <div>{content}</div>
     </div>
   );
 };
