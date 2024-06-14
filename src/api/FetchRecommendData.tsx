ffd35fc3485f238a35fb8916b0d2109688cf8ebe
summermong <2514o_o@naver.com>
2024-06-14 23:55:21 +0900

    :sparkles: FEAT: 추천 페이지 api

---
+++ src/api/FetchRecommendData.tsx
@@ -0,0 +1,23 @@
+import { PlaceRecommendData } from '../interface/PlaceRecommendData';
+import { axiosInstance } from './axiosInstance';
+
+const FetchRecommendData = async (
+  latitude: number,
+  longitude: number
+): Promise<PlaceRecommendData> => {
+  if (latitude === null || longitude === null) {
+    throw new Error('위치 정보가 없습니다.');
+  }
+
+  const response = await axiosInstance.get<PlaceRecommendData>(
+    `api/v1/recommend?latitude=${latitude}&longitude=${longitude}`
+  );
+
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw new Error('Unexpected response status');
+  }
+};
+
+export default FetchRecommendData;
