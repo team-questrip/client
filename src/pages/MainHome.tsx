c1a8e5b7bee8a8041661af4361f3d5d95ae93181
confidential-nt <youyoy689@gmail.com>
2024-08-28 16:48:33 +0900

    feat: Home 컴포넌트 수정
    - 경로를 /main-home에서 /로 수정
    - 반응성 수정

--- src/pages/MainHome.tsx
+++ src/pages/MainHome.tsx
@@ -1,55 +1,57 @@
-import HomeCard from "../components/HomeCard";
+import HomeCard from '../components/HomeCard';
 
-const Main = () => {
-    const handleClick = () => {
-        console.log("Button Clicked!")
-    };
+function Home() {
+  const handleClick = () => {
+    console.log('Button Clicked!');
+  };
 
-    return (
-        <>
-            <header className="flex justify-center h-12">
-                <img src="/img/logo-horizontal.png" className="h-8" />
-            </header>
-            <div className="w-full h-full p-4 flex-col grid grid-cols-2 gap-3">
-                <HomeCard
-                    img={"/icons/food_and_drink.png"}
-                    category={"Food & Drinks"}
-                    cnt={79}
-                    onClick={handleClick}                
-                />
-                <HomeCard
-                    img={"/icons/culture_and_history.png"}
-                    category={"Culture & History"}
-                    cnt={19}
-                    onClick={handleClick}                
-                />
-                <HomeCard
-                    img={"/icons/nature_and_landmarks.png"}
-                    category={"Nature & Landmarks"}
-                    cnt={15}
-                    onClick={handleClick}                
-                />
-                <HomeCard
-                    img={"/icons/shopping_and_entertainment.png"}
-                    category={"Shopping & Entertainments"}
-                    cnt={32}
-                    onClick={handleClick}                
-                />
-                <HomeCard
-                    img={"/icons/wellness_and_relaxation.png"}
-                    category={"Wellness & Relaxation"}
-                    cnt={9}
-                    onClick={handleClick}                
-                />
-                <HomeCard
-                    img={"/icons/day_tours_and_activities.png"}
-                    category={"Day Tours & Activitites"}
-                    cnt={51}
-                    onClick={handleClick}                
-                />
-            </div>
-        </>
-    )
-};
+  return (
+    <>
+      <header className="flex justify-center h-12">
+        <a href="/">
+          <img src="/img/logo-horizontal.png" className="h-8" />
+        </a>
+      </header>
+      <div className="w-full h-full p-4 flex-col grid grid-cols-2 gap-3">
+        <HomeCard
+          img={'/icons/food_and_drink.png'}
+          category={'Food & Drinks'}
+          cnt={79}
+          onClick={handleClick}
+        />
+        <HomeCard
+          img={'/icons/culture_and_history.png'}
+          category={'Culture & History'}
+          cnt={19}
+          onClick={handleClick}
+        />
+        <HomeCard
+          img={'/icons/nature_and_landmarks.png'}
+          category={'Nature & Landmarks'}
+          cnt={15}
+          onClick={handleClick}
+        />
+        <HomeCard
+          img={'/icons/shopping_and_entertainment.png'}
+          category={'Shopping & Entertainments'}
+          cnt={32}
+          onClick={handleClick}
+        />
+        <HomeCard
+          img={'/icons/wellness_and_relaxation.png'}
+          category={'Wellness & Relaxation'}
+          cnt={9}
+          onClick={handleClick}
+        />
+        <HomeCard
+          img={'/icons/day_tours_and_activities.png'}
+          category={'Day Tours & Activitites'}
+          cnt={51}
+          onClick={handleClick}
+        />
+      </div>
+    </>
+  );
+}
 
-export default Main;
+export default Home;
