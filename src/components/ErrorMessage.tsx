202dbf2e9c1b0b9c5bc76cc09da755b1ff6f9db0
confidential-nt <youyoy689@gmail.com>
2024-08-29 18:58:36 +0900

    feat: nps 페이지에서 점수 required 되도록 수정

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,5 +1,5 @@
 interface ErrorMessageProps {
-  message?: string;
+  message?: string | null;
 }
 
 const ErrorMessage = ({ message }: ErrorMessageProps) => {
--- src/pages/NPS.tsx
+++ src/pages/NPS.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import Button from '../components/Button';
 import GoBackHeader from '../components/GoBackHeader/GoBackHeader';
 import { useNavigate } from 'react-router-dom';
+import ErrorMessage from '../components/ErrorMessage';
 
 const NPSForm = () => {
-  const [npsScore, setNpsScore] = useState<number | undefined>(undefined);
+  const [npsScore, setNpsScore] = useState<number>();
   const [feedback, setFeedback] = useState('');
   const [charCount, setCharCount] = useState(0);
+  const [error, setError] = useState<string | null>();
 
   const handleNpsChange = (score: number) => {
+    setError(null);
     setNpsScore(score);
   };
 
@@ -18,8 +21,14 @@ const NPSForm = () => {
     setCharCount(inputValue.length);
   };
 
-  const handleSubmit = () => {
-    alert(`Submitted NPS: ${npsScore}, Feedback: ${feedback}`);
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (npsScore === undefined) {
+      setError('This is required.');
+    }
+
+    // todo: api 요청 보내기
   };
 
   const navigate = useNavigate();
@@ -30,45 +39,42 @@ const NPSForm = () => {
   return (
     <>
       <GoBackHeader onBack={handleBack} className="mb-5" />
-      <div className="text-xl font-bold mb-4">
-        How would you introduce Questrip app to your friends who are visiting
-        Korea?
-      </div>
-      <div className="flex flex-wrap justify-start items-start gap-2.5 mb-6">
-        {Array.from({ length: 12 }, (_, index) => (
-          <button
-            key={index}
-            className={`px-4 py-1.5 flex-col rounded-full justify-center items-center ${
-              npsScore === index
-                ? 'bg-primary text-white'
-                : 'bg-secondaryBackground'
-            } hover:bg-primary`}
-            onClick={() => handleNpsChange(index)}
-          >
-            {index}
-          </button>
-        ))}
-      </div>
-
-      <div className="text-xl font-bold mb-4">
-        Any messages that you would like to give? (optional)
-      </div>
-      <div className="text-right text-sm text-hintText mb-4">
-        {charCount}/500
-      </div>
-      <textarea
-        className="w-full h-24 p-4 border border-hintText rounded mb-2"
-        placeholder="Feel free to send us your opinions"
-        value={feedback}
-        onChange={handleFeedbackChange}
-        maxLength={500}
-      />
-      <Button
-        onClick={handleSubmit}
-        type="submit"
-        text="Submit"
-        className="mt-32"
-      />
+      <form onSubmit={handleSubmit}>
+        <div className="text-xl font-bold mb-4">
+          How would you introduce Questrip app to your friends who are visiting
+          Korea?
+        </div>
+        <div className="flex flex-wrap justify-start items-start gap-2.5 mb-2">
+          {Array.from({ length: 12 }, (_, index) => (
+            <button
+              key={index}
+              className={`px-4 py-1.5 flex-col rounded-full justify-center items-center ${
+                npsScore === index
+                  ? 'bg-primary text-white'
+                  : 'bg-secondaryBackground'
+              } hover:bg-primary`}
+              onClick={() => handleNpsChange(index)}
+            >
+              {index}
+            </button>
+          ))}
+        </div>
+        <ErrorMessage message={error} />
+        <div className="text-xl font-bold mb-4">
+          Any messages that you would like to give? (optional)
+        </div>
+        <div className="text-right text-sm text-hintText mb-4">
+          {charCount}/500
+        </div>
+        <textarea
+          className="w-full h-24 p-4 border border-hintText rounded mb-2 resize-none"
+          placeholder="Feel free to send us your opinions"
+          value={feedback}
+          onChange={handleFeedbackChange}
+          maxLength={500}
+        />
+        <Button type="submit" text="Submit" className="mt-32" />
+      </form>
     </>
   );
 };
