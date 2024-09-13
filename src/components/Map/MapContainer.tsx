94cf560282ad3f971d3ea59a7d210e16981c8d25
confidential-nt <youyoy689@gmail.com>
2024-09-13 17:04:40 +0900

    style: 주석 및 필요없는 코드 제거

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,4 +1,4 @@
-import { Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import { Map } from '@vis.gl/react-google-maps';
 import PlaceMarkers from './PlaceMarkers';
 import { UserCurrentPosition } from '../../types/current-position';
 import { Place } from '../../types/place';
@@ -18,7 +18,7 @@ const MapContainer = ({ userCurrentPosition }: MapContainerProps) => {
 
   const { placeData, fetchNextPage, hasNextPage } =
     usePlaceInfiniteQuery(userCurrentPosition);
-  // todo: 장소 더 가져오기
+
   const onClickMarker = (place: Place) => {
     setSelectedPlace(place);
   };
@@ -35,14 +35,7 @@ const MapContainer = ({ userCurrentPosition }: MapContainerProps) => {
           lng: userCurrentPosition.longitude,
         }}
         mapId={import.meta.env.VITE_Google_MAP_ID}
-        onCameraChanged={(ev: MapCameraChangedEvent) =>
-          console.log(
-            'camera changed:',
-            ev.detail.center,
-            'zoom:',
-            ev.detail.zoom
-          )
-        }
+        onCameraChanged={() => {}}
       >
         {placeData && (
           <PlaceMarkers
@@ -63,7 +56,7 @@ const MapContainer = ({ userCurrentPosition }: MapContainerProps) => {
       )}
       <Button
         text="more places"
-        className="w-1/2 absolute top-20 left-1/2 -translate-x-1/2"
+        className="!w-1/2 absolute top-20 left-1/2 -translate-x-1/2"
         onClick={() => {
           if (hasNextPage) {
             fetchNextPage();
